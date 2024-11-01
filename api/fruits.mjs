770df9c1c30bb319da3bb1924c9b7687c4b9115b
Muhammad Faizan Uddin <faizan.uddin94@gmail.com>
2024-11-01 06:07:31 -0600

    added fruits.mjs and require

--- api/fruits.mjs
+++ api/fruits.mjs
@@ -5,22 +5,15 @@ export default async function handler(req, res) {
     const response = await fetch(
       'https://wcz3qr33kmjvzotdqt65efniv40kokon.lambda-url.us-east-2.on.aws/'
     );
-
-    if (!response.ok) {
-      throw new Error(
-        `External API request failed with status ${response.status}`
-      );
-    }
+    if (!response.ok) throw new Error(`Fetch error: ${response.status}`);
 
     const data = await response.json();
-
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET');
     res.setHeader('Content-Type', 'application/json');
-
     res.status(200).json(data);
   } catch (error) {
-    console.error('Error in serverless function:', error);
+    console.error('Error:', error);
     res.status(500).json({ message: 'Server Error', error: error.message });
   }
 }
