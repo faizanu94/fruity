0037c681ceff652cbe0d204610e915fd27971dc2
Muhammad Faizan Uddin <faizan.uddin94@gmail.com>
2024-11-01 06:04:25 -0600

    added node-fetch

--- api/fruits.js
+++ api/fruits.js
@@ -1,14 +1,26 @@
 import fetch from 'node-fetch';
 
 export default async function handler(req, res) {
-  const response = await fetch(
-    'https://wcz3qr33kmjvzotdqt65efniv40kokon.lambda-url.us-east-2.on.aws/'
-  );
-  const data = await response.json();
+  try {
+    const response = await fetch(
+      'https://wcz3qr33kmjvzotdqt65efniv40kokon.lambda-url.us-east-2.on.aws/'
+    );
 
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET');
-  res.setHeader('Content-Type', 'application/json');
+    if (!response.ok) {
+      throw new Error(
+        `External API request failed with status ${response.status}`
+      );
+    }
 
-  res.status(200).json(data);
+    const data = await response.json();
+
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET');
+    res.setHeader('Content-Type', 'application/json');
+
+    res.status(200).json(data);
+  } catch (error) {
+    console.error('Error in serverless function:', error);
+    res.status(500).json({ message: 'Server Error', error: error.message });
+  }
 }
